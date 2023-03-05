22ac8475dd1de9ea026f3a438c40fd2563cce9ea
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-03-05 12:45:55 +0200

    add styles

--- src/components/Styles/Styles.js
+++ src/components/Styles/Styles.js
@@ -10,26 +10,7 @@ export const Container = styled.div`
     grid-gap: 16px;
     padding-bottom: 24px;
 ` 
-  
-  /*
-   * Стили компонента ImageGalleryItem
-   */
-export const ImageGalleryItem = styled.li`
-    border-radius: 2px;
-    box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
-      0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
-`
-export const ImageGalleryItemImage = styled.img`
-    width: 100%;
-    height: 260px;
-    object-fit: cover;
-    transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
-    &:hover {
-        transform: scale(1.03);
-        cursor: zoom-in;
-    }
-`
   /*
    * Стили компонента Modal
    */
@@ -50,36 +31,4 @@ export const Modal = styled.div`
     max-width: calc(100vw - 48px);
     max-height: calc(100vh - 24px);
 `
-  
-  /*
-  * Стили компонента Buton (Load more)
-  */
-  export const Button = styled.button`
-    padding: 8px 16px;
-    border-radius: 2px;
-    background-color: #3f51b5;
-    transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
-    text-align: center;
-    display: inline-block;
-    color: #fff;
-    border: 0;
-    text-decoration: none;
-    cursor: pointer;
-    font-family: inherit;
-    font-size: 18px;
-    line-height: 24px;
-    font-style: normal;
-    font-weight: 500;
-    min-width: 180px;
-    box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
-      0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
-
-      &:hover,
-      &:focus {
-            background-color: #303f9f;
-      }
-  `
-  
-  
-  
   
