b113c1bd376335ae80e0825b7c00283c7f33321d
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-03-05 18:34:45 +0200

    update button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { getImages } from "components/services/getImages";
 
 import { Hearts } from  'react-loader-spinner'
 
+
 import { Container } from "./Styles/Styles";
 import { Searchbar } from "./Searchbar/Searchbar";
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -14,6 +15,7 @@ export class App  extends Component{
   state = {
 		textSearch: '',
     images: [],
+    totalHits: 0,
 		error: '',
 		page: 1,
     isLoading: false,
@@ -24,24 +26,24 @@ export class App  extends Component{
       if (prevState.textSearch !== this.state.textSearch){
         this.setState({images: []});
       }
-      this.setState({ isLoading: true });
+      this.setState({ isLoading: true});
 
       try{
         getImages(this.state.textSearch, this.state.page)
         .then((data) =>  {
           this.setState({
 						images: [...this.state.images, ...data.hits],
+            totalHits: data.totalHits,
 					})
           if(data.total === 0){
             this.setState({ error:  "Sorry, there are no images matching your search query. Please try again."});
-            throw new Error("Sorry, there are no images matching your search query. Please try again.");
           }
         });
       } catch(error) {
         this.setState({ error: error.message });
       } finally {
 
-        setTimeout(()=>this.setState({ isLoading: false}), 1000);
+        setTimeout(()=>this.setState({ isLoading: false}), 300);
         
       }
     }
@@ -49,7 +51,7 @@ export class App  extends Component{
 }
 
 	handleSubmit = (textSearch) => {
-		this.setState({ textSearch})
+		this.setState({ textSearch, error: ""})
 	}
 
   loadMore = ()=>{
@@ -63,19 +65,18 @@ export class App  extends Component{
         
         {this.state.isLoading && 
         <Hearts 
-        height="80"
-        width="80"
-        color="#4fa94d"
-        ariaLabel="hearts-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
+              height="80"
+              width="80"
+              color="#a94d69"
+              ariaLabel="hearts-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
       />}
         <ImageGallery data={this.state.images}/>
 
-        {Boolean(this.state.images.length) &&
-          <ButtonLoadMore  onClick={this.loadMore}/>
-        }
+        {(this.state.page * 12 <= this.state.totalHits) &&
+            <ButtonLoadMore  onClick={this.loadMore}/>}
 
         {Boolean(this.state.error.length) && <p>{this.state.error}</p>}
         
