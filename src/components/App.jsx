93eb2eab24d140daa53602b82d4174e3080f7c6c
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-03-11 17:46:34 +0200

    bugfix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -30,10 +30,10 @@ export class App  extends Component{
       try{
         getImages(this.state.textSearch, this.state.page)
         .then((data) =>  {
-          this.setState({
-						images: [...this.state.images, ...data.hits],
+          this.setState((prevState)=>({
+						images: [...prevState.images, ...data.hits],
             totalHits: data.totalHits,
-					})
+					}))
           if(data.total === 0){
             this.setState({ error:  "Sorry, there are no images matching your search query. Please try again."});
           }
@@ -49,6 +49,10 @@ export class App  extends Component{
 }
 
 	handleSubmit = (textSearch) => {
+    if (textSearch === this.state.textSearch && this.state.page === 1) {
+      alert("Images already showed")
+      return;
+    }
 		this.setState({ textSearch, error: "", page: 1, totalHits: 0, images: []})
 	}
 
